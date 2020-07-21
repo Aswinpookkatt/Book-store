dc2d73ecd0962f3cc230daae09be9af5830dedd9
Aswin C P <aswin.p1800@gmail.com>
2020-07-21 13:47:21 +0530

    updated views.py

--- bookapp/Book/views.py
+++ bookapp/Book/views.py
@@ -6,42 +6,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your views here.
-#def home(request):
-#    ebooks = Ebook.objects.order_by('-id')[:8]
-#    return render(request, 'ebook/home.html', {
-#        'ebooks' : ebooks
-#    })
 def homepage(request):
     ebooks = Ebook.objects.order_by('-id')[:8]
     return render(request, 'ebook/homepage.html', {
         'ebooks' : ebooks
     })
-#def categories(request):
-#    categories = Category.objects.order_by('name')
-#    return render(request, 'ebook/categories.html', {
-#		'categories': categories,
-#		})
+
 def categories_(request):
     categories = Category.objects.order_by('name')
     return render(request, 'ebook/categories_.html', {
 		'categories': categories,
 		})
-
-#def category(request, category_id):
-#    category = get_object_or_404(Category, pk = category_id)
-#    ebooks = category.ebook_set.all()
-#
-#    paginator = Paginator(ebooks, 12)
-#    try:
-#        page = request.GET['page']
-#    except:
-#        page = 1
-#    ebooks = paginator.get_page(page)
-#
-#    return render(request, 'ebook/category.html', {
-#		'category' : category,
-#		'ebooks' : ebooks
-#		})
 
 def category_(request, category_id):
     category = get_object_or_404(Category, pk = category_id)
@@ -59,18 +34,6 @@ def category_(request, category_id):
 		'ebooks' : ebooks
 		})
 
-
-#def ebooks(request):
-#    ebooks = Ebook.objects.all()
-#    paginator = Paginator(ebooks, 5)
-#    try:
-#        page = request.GET['page']
-#    except:
-#        page = 1
-#    ebooks = paginator.get_page(page)
-#    return render(request, 'ebook/ebooks.html', {
-#		'ebooks': ebooks,
-#		})
 
 def ebooks_(request):
     ebooks = Ebook.objects.all()
@@ -91,24 +54,6 @@ def ebook(request, ebook_id):
 		'ebook' : ebook,
         'comments' : comments,
 		})
-
-#def comment(request, ebook_id):
-#    if request.method == 'POST':
-#        user = auth.get_user(request)
-#        ebook = get_object_or_404(Ebook, pk = ebook_id)
-
-        # create the comment
-#        comment = Comment()
-#        comment.body = request.POST['body']
-#        comment.pub_time = timezone.datetime.now()
-#        comment.ebook = ebook
-#        comment.user = user
-#        comment.save()
-
-#        return redirect('ebook', ebook_id)
-#    else:
-#        return redirect('home')
-
 
 def ebook_(request, ebook_id):
     ebook = get_object_or_404(Ebook, pk = ebook_id)
@@ -138,24 +83,6 @@ def comment(request, ebook_id):
 
 #user authentication
 
-#def signup(request):
-#    if request.method == 'POST':
-#        # sign up the user
-#        if request.POST['password1'] == request.POST['password2']:
-#            try:
-#                user = User.objects.get(username=request.POST['username'])
-#                return render(request, 'ebook/signup.html', {'error': 'Username is already taken!'})
-#            except User.DoesNotExist:
-#                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-#                auth.login(request, user)
-#                return render(request, 'ebook/home.html', {'success': 'You are successfully registered and logged in!'})
-#        else:
-#            return render(request, 'ebook/signup.html', {'error': 'Passwords aren\'t matched!'})
-#    else:
-#        # user wants to sign up
-#        return render(request, 'ebook/signup.html')
-
-
 def signup_(request):
     if request.method == 'POST':
         # sign up the user
@@ -173,20 +100,6 @@ def signup_(request):
         # user wants to sign up
         return render(request, 'ebook/signup_.html')
 
-
-
-#def login(request):
-#    if request.method == 'POST':
-        # login user
-    #    user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
-    #    if user is not None:
-    #        auth.login(request, user)
-    #        return render(request, 'ebook/home.html', {'success': 'You are successfully logged in!'})
-    #    else:
-    #        return render(request, 'ebook/login.html', {'error': 'Username/Password doesn\'t match'})
-    #else:
-        # user wants to login
-    #    return render(request, 'ebook/login.html')
 
 def login_(request):
     if request.method == 'POST':
